25c0b2c09a8ba79f8705a8bd710b27a2e846b890
veeravallisubash <155515797+veeravallisubash@users.noreply.github.com>
2025-07-11 10:16:14 +0530

    Create 7.rs

---
+++ 7.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let my_tuple = (42, 3.14, "Rust");
+
+    let first = my_tuple.0;
+    let second = my_tuple.1;
+    let third = my_tuple.2;
+
+    println!("Tuple values:");
+    println!("First: {}", first);
+    println!("Second: {}", second);
+    println!("Third: {}", third);
+}
