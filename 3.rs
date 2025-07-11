ebcffe85b88651958eff375cf32a41cfe3fadba4
veeravallisubash <155515797+veeravallisubash@users.noreply.github.com>
2025-07-11 10:07:11 +0530

    Create 3.rs

---
+++ 3.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut x = 1000;
+
+    let y = "Programming";
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+
+    x = 1100;
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+}
